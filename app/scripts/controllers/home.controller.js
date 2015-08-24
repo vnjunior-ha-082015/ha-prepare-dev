2a7b69a9a7e90e30bf71bff11c459c60dac713e6
freedom9x <hien pham>
2015-08-24 23:59:59 +0400

    home page

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -8,11 +8,23 @@
  * Controller of the hackathonApp
  */
 angular.module('hackathonApp')
-  .controller('HomeController', function($scope){
+  .controller('HomeController', function($scope, commonShareService){
     
   	$scope.myInterval = false;
   	$scope.noWrapSlides = true;
-  	$scope.slides = [{image:'images/dubai-img/attlantis-1.jpg'}, {image:'images/dubai-img/attlantis-2.jpg'},
-  					{image:'images/dubai-img/attlantis-3.jpg'}];
+  	$scope.slides;
+  	$scope.selectedCircle ;
 
+  	commonShareService.getDestination().then(function(response){
+  		$scope.des = response;
+  		// $scope.selectedCircle = $scope.des[0];
+  		$scope.slides = response[0].photoList;
+	});
+
+  	$scope.select = function(id){
+  		$scope.slides = $scope.des[9].photoList;
+  		// $scope.selectedCircle = $scope.des[id-1];
+  		// console.log("select " + id);
+  		// $scope.$apply();
+  	}
   });
