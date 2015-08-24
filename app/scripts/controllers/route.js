1348d92594d7f7e34503f6fb85777dd8bd5d199c
Luu Le <luu.le@dnata.com>
2015-08-24 20:44:29 +0400

    create route

--- app/scripts/controllers/route.js
+++ app/scripts/controllers/route.js
@@ -81,21 +81,17 @@
 	    $mdDialog.hide(answer);
 	  };
 
-	  $scope.simulateQuery = false;
-      $scope.repos         = loadAll();
+      $scope.newDestinationsList = [{
+									"destination": 'TEST',
+							        "address": null,
+							        "description": null,
+							        "longtt": null,
+							        "latt": null,
+							        "photo": null
+							      }];
       $scope.getDestinationsList   = getDestinationsList;
       $scope.selectedDestinationChange = selectedDestinationChange;
-	  // function getDestinationsList (query) {
-	  //     var results = query ? $scope.repos.filter( createFilterFor(query) ) : $scope.repos,
-	  //         deferred;
-	  //     if ($scope.simulateQuery) {
-	  //       deferred = $q.defer();
-	  //       $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
-	  //       return deferred.promise;
-	  //     } else {
-	  //       return results;
-	  //     }
-	  //   };
+	  
 
 	   function getDestinationsList (query) {
 	      var promise = commonShareService.getDestination(query);
@@ -105,57 +101,8 @@
 	    };
 
 	    function selectedDestinationChange(item) {
-	      $log.info('Item changed to ' + JSON.stringify(item));
+	    	$scope.newDestinationsList.push(item);
 	    }
-
-	    function loadAll() {
-		      var repos = [
-		        {
-		          'name'      : 'Angular 1',
-		          'url'       : 'https://github.com/angular/angular.js',
-		          'watchers'  : '3,623',
-		          'forks'     : '16,175',
-		        },
-		        {
-		          'name'      : 'Angular 2',
-		          'url'       : 'https://github.com/angular/angular',
-		          'watchers'  : '469',
-		          'forks'     : '760',
-		        },
-		        {
-		          'name'      : 'Angular Material',
-		          'url'       : 'https://github.com/angular/material',
-		          'watchers'  : '727',
-		          'forks'     : '1,241',
-		        },
-		        {
-		          'name'      : 'Bower Material',
-		          'url'       : 'https://github.com/angular/bower-material',
-		          'watchers'  : '42',
-		          'forks'     : '84',
-		        },
-		        {
-		          'name'      : 'Material Start',
-		          'url'       : 'https://github.com/angular/material-start',
-		          'watchers'  : '81',
-		          'forks'     : '303',
-		        }
-		      ];
-		      return repos.map( function (repo) {
-		        repo.value = repo.name.toLowerCase();
-		        return repo;
-		      });
-		    };
-
-		/**
-	     * Create filter function for a query string
-	     */
-	    function createFilterFor(query) {
-	      var lowercaseQuery = angular.lowercase(query);
-	      return function filterFn(item) {
-	        return (item.value.indexOf(lowercaseQuery) === 0);
-	      };
-	    };
 
 	};
 
