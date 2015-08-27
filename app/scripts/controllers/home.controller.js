34dacfe700de663c6e36135cb531571633b5939b
hienpham2991 <hienpham2991@gmail.com>
2015-08-28 00:00:09 +0400

    home page

--- app/scripts/controllers/home.controller.js
+++ app/scripts/controllers/home.controller.js
@@ -10,47 +10,38 @@
  angular.module('hackathonApp')
  .controller('HomeController', function($scope, $rootScope, $compile, commonShareService){
 
-  	$rootScope.activeTab = 'home';
-  	$scope.slides;
-  	$scope.mySlides = [];
-  	$scope.selectedCircle  = 1;
-  	$scope.isSelected = function(id){
-  		if (id == $scope.selectedCircle) return true;
-  		else return false;
-  	}
-  	$scope.des = commonShareService.getDestination();
-  	for(var i = 0; i < $scope.des[0].photoList.length; i++){
-    		$scope.mySlides.push({image:$scope.des[0].photoList[i], active:false});
-  	}
-  	$scope.mySlides[0].active = true;
-  	$scope.description = $scope.des[0].description;
-  	$scope.select = function(id){
-  		$scope.selectedCircle  = id;
-  		$scope.description = $scope.des[id-1].description;
-  		$scope.mySlides = [];
-  		for(var i = 0; i < $scope.des[id-1].photoList.length; i++){
-  			$scope.mySlides.push({image:$scope.des[id-1].photoList[i], active:false});
-  		}
-  		$scope.mySlides[0].active = true;
+   $rootScope.activeTab = 'home';
+   $scope.selectedCircle  = 1;
+   $scope.des = commonShareService.getDestination();
+   $scope.selectedObj = $scope.des[0];
 
-  		var htmlStr = '<carousel interval="myInterval" no-wrap="noWrapSlides">'
-  		+ '  <slide ng-repeat="slide in mySlides">'
-  		+ '    <img ng-src="{{slide.image}}" active="slide.active" width="600" height="300" style="margin:auto; height: 300px">'
-  		+ '    <div class="carousel-caption">'
-  		+ '    </div>'
-  		+ '  </slide>'
-  		+ '</carousel>';
 
-  		var elem = $compile(angular.element(htmlStr))($scope);
-  		var carouselSection = document.getElementById('carousel-section');
-  		carouselSection.innerHTML = '';
-  		angular.element(carouselSection).append(elem);
+   $scope.select = function(id){
+   $scope.selectedCircle  = id;
+  $scope.selectedObj = $scope.des[id-1];
 
-  		setTimeout(function() {
-  			angular.element(document.querySelector('#carousel-section .carousel-indicators')).removeClass('ng-hide');
-  			angular.element(document.querySelector('#carousel-section .left.carousel-control')).removeClass('ng-hide');
-  			angular.element(document.querySelector('#carousel-section .right.carousel-control')).removeClass('ng-hide');
-  		}, 100);
+    var htmlStr = '<carousel interval="myInterval" no-wrap="noWrapSlides">'
+    + '  <slide ng-repeat="slide in selectedObj.photoList">'
+    + '    <img ng-src="{{slide}}"  width="600" height="300" style="margin:auto; height: 300px">'
+    + '    <div class="carousel-caption">'
+    + '    </div>'
+    + '  </slide>'
+    + '</carousel>';
 
-  	}
-  });
+    var elem = $compile(angular.element(htmlStr))($scope);
+    var carouselSection = document.getElementById('carousel-section');
+    carouselSection.innerHTML = '';
+    angular.element(carouselSection).append(elem);
+
+    setTimeout(function() {
+     angular.element(document.querySelector('#carousel-section .carousel-indicators')).removeClass('ng-hide');
+     angular.element(document.querySelector('#carousel-section .left.carousel-control')).removeClass('ng-hide');
+     angular.element(document.querySelector('#carousel-section .right.carousel-control')).removeClass('ng-hide');
+   }, 100);
+
+    $scope.isSelected = function(id){
+      if (id == $scope.selectedCircle) return true;
+      else return false;
+    }
+  }
+});
